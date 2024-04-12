91db42a05c7283f1413384e7e8ab0ebe24ed6ff8
unknown <danielkksilva1@gmail.com>
2024-04-12 16:06:05 -0400

    lógica posts implementada

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,36 +9,53 @@ function redirectPost(postid) {
 document.addEventListener("DOMContentLoaded", () => {
 
     const contCards = document.querySelector(".container-cards");
+    const catCards = document.querySelector(".container-categ")
 
     fetch("./assets/posts.json")
         .then(response => response.json())
         .then(data => {
             let totalItems = data.length;
             let items = [];
-            while (items.length <= 6) {
-                let value = getRandomInt(totalItems);
+            let recomendados = [];
+            while (items.length != 4) {
+                let value = getRandomInt(totalItems)
                 if (!items.includes(value)) {
                     items.push(value);
                 }
             }
+            while (recomendados.length != 3) {
+                let value = getRandomInt(totalItems);
+                if (!items.includes(value) && !recomendados.includes(value)) {
+                    recomendados.push(value);
+                }
+            }
             console.log(items)
+            console.log(recomendados)
             data.forEach((post) => {
-                items = items.sort();
+                console.log(post.id)
                 items.forEach((item) => {
                     if (post.id == item) {
-                        const img_url = post.img_url.split("../");
-                        contCards.innerHTML += createCard(post.id, post.category, post.title, img_url[1]);
+                        let img_url = post.img_url.split("../");
+                        let cards = createCard(post.id, post.category, post.title, img_url[1]);
+                        contCards.innerHTML += cards[0];
+                    }
+                });
+                recomendados.forEach((item) => {
+                    if (post.id == item) {
+                        let img_url = post.img_url.split("../");
+                        let cards = createCard(post.id, post.category, post.title, img_url[1], post.description);
+                        catCards.innerHTML += cards[1];
                     }
                 });
             });
         });
 
-    function createCard(postid, cat, titulo, img_url) {
-        let card = `
+    function createCard(postid, cat, titulo, img_url, description) {
+        let card1 = `
         <div class="card-custom" style="background-image: url('${img_url}')">
             <div class="card-custom-top w-100">
                 <a href="#" class="text text-start px-1"
-                    style="color: #DDBEA9; text-decoration: none;">${cat}</a>
+                    style="color: #DDBEA9; text-decoration: none;" onClick="redirectCategory('${cat}')">${cat}</a>
                 <a href="#" style="text-decoration: none;" onClick="redirectPost(${postid})">
                     <h4 class="text-light">${titulo}</h4>
                 </a>
@@ -56,7 +73,24 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         </div>
         `;
-        return card;
+
+        let card2 = `
+        <div class="container cat-card mt-2 d-flex-col d-flex gap-2 p-3 justify-content-center align-content-center flex-lg-row align-items-center">
+            <img src="${img_url}" alt="" class="img-fluid rounded-3"
+                style="height: 200px;">
+            <div class="container mt-2 mt-lg-0 d-flex flex-column">
+                <h4>${titulo}</h4>
+                <p>${description}</p>
+                <a href="#" class="btn btn-primary" style="width: 8rem" onClick="redirectPost(${postid})">Acesse</a>
+                <hr>
+                <a href="#" class="text-black" style="text-decoration: none;" onClick="redirectCategory('${cat}')">${cat}</a>
+            </div>
+        </div>
+        `;
+
+        const cards = [card1, card2]
+        
+        return cards;
     }
 
     function getRandomInt(max) {
